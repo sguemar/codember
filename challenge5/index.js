2f441bcb17c12f331788885f8cb2d8cb6fac6454
Samuel <samuguemar@gmail.com>
2022-12-04 20:57:53 +0000

    feat: add challenge 5 demo

---
+++ challenge5/index.js
@@ -0,0 +1,8 @@
+import { readFileSync } from 'node:fs'
+import { battleRoyale } from './challenge5.js'
+
+const patrons = JSON.parse(readFileSync('challenge5/patrons.json', 'utf-8'))
+
+const survivor = battleRoyale(patrons)
+
+console.log(survivor)
