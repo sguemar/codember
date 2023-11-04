a3cb598ff730d108bc0546a4c0945412a317e5d7
Samuel <samuguemar@gmail.com>
2023-11-04 22:58:04 +0000

    fix: paths of data files

--- 2022/challenge3/index.js
+++ 2022/challenge3/index.js
@@ -1,7 +1,7 @@
 import { readFileSync } from 'node:fs'
 import { getLongestZebra } from './challenge3.js'
 
-const colors = JSON.parse(readFileSync('challenge3/colors.txt', 'utf-8'))
+const colors = JSON.parse(readFileSync('2022/challenge3/colors.txt', 'utf-8'))
 
 const longestZebra = getLongestZebra(colors)
 
--- 2022/challenge5/index.js
+++ 2022/challenge5/index.js
@@ -1,7 +1,7 @@
 import { readFileSync } from 'node:fs'
 import { battleRoyale } from './challenge5.js'
 
-const patrons = JSON.parse(readFileSync('challenge5/patrons.json', 'utf-8'))
+const patrons = JSON.parse(readFileSync('2022/challenge5/patrons.json', 'utf-8'))
 
 const survivor = battleRoyale(patrons)
 
